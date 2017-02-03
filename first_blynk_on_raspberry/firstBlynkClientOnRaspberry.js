cb023701e8bd92e269c78b32b8340e7a189984d1
Carl Lundin <carl.a.j.lundin@gmail.com>
2017-02-03 21:42:11 +0000

    First blynk on raspberry pi

---
+++ first_blynk_on_raspberry/firstBlynkClientOnRaspberry.js
@@ -0,0 +1,34 @@
+// V1 input pin. Blynk app virtual button issues voice command on Raspberry Pi.
+// V3 output pin. Pin controls Blynk app virtual LED, on/off every 200ms.
+// V9 output pin. Pin controls Blynk app virtaul display, sends counter value.
+
+var BlynkLib = require('blynk-library');
+var http = require('http');
+
+var blynk = new BlynkLib.Blynk('432a7c60ab2f49eb979a5a891109dae4');
+var v1 = new blynk.VirtualPin(1); 
+var v3 = new blynk.VirtualPin(3); 
+var v9 = new blynk.VirtualPin(9);
+
+// Write to LED in Blynk app every 200ms
+var on = false;
+setInterval(function(){
+  if (on) {
+    v3.write(255);
+  } else {
+    v3.write(0);
+  }
+  
+  on = on == false;
+}, 200);
+
+// Blynk app writes on button press
+v1.on('write', function(param) {
+  http.get('http://localhost:5005/Mat/say/HOHO/sv-se/30', (res) => { console.log("Voice command OK"); });
+});
+
+// Blynk app reads every X time interval (set time in app)
+var counter = 0;
+v9.on('read', function() {
+  v9.write(counter++);
+});
